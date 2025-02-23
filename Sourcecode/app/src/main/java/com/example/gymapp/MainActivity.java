208f964ae8523d83da583cda4bc9ce3a2d30d55e
LiamJones05 <liamajones2005@gmail.com>
2025-02-23 13:46:02 +0000

    Basic Setup

---
+++ Sourcecode/app/src/main/java/com/example/gymapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.gymapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public class WebAppInterface{
+        private MainActivity activity;
+        public WebAppInterface(MainActivity activity){
+            this.activity = activity;
+        }
+
+        @JavascriptInterface
+        public void showToast(String message){
+            activity.runOnUiThread(() ->
+                    Toast.makeText(activity,message,Toast.LENGTH_SHORT).show()
+            );
+        }
+    }
+}
